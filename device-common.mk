532a414bc1117c0f0b6e1f1870e2199bdf5de1a6
Kevin Hilman <khilman@baylibre.com>
2021-03-01 13:59:49 -0800

    Revert "pkvm: Add virt APEX to yukawa builds"
    This reverts commit a9d21fa6d2745b736c51a55eda76b77db9e706cb.
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Change-Id: Ice243149bafdf19fd8308b524bba4952a3ba6d52

--- device-common.mk
+++ device-common.mk
@@ -156,7 +156,7 @@ endif
 
 PRODUCT_PACKAGES += \
     libhidltransport \
-    libhwbinder 
+    libhwbinder
 
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 
@@ -325,7 +325,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml 
+    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
 AUDIO_DEFAULT_OUTPUT ?= speaker
 ifeq ($(AUDIO_DEFAULT_OUTPUT),hdmi)
@@ -363,6 +363,3 @@ PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-external-service
 PRODUCT_COPY_FILES += \
     device/amlogic/yukawa/hal/camera/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
-# Include Virtualization APEX
-$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
